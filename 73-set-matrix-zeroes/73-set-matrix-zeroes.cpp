4e0fb3a5bf8c055f54453a4122aea926fae0d744
Shambhavi Gupta <104186704+1103shambhavi@users.noreply.github.com>
2022-07-29 08:26:08 +0530

    Time: 18 ms (73.54%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,68 +1,48 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) 
+    void setZeroes(vector<vector<int>>& M) 
     {
-        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-
-    for (int i = 0; i < rows; i++) {
-        if (matrix[i][0] == 0) col0 = 0;
-        for (int j = 1; j < cols; j++)
-            if (matrix[i][j] == 0)
-                matrix[i][0] = matrix[0][j] = 0;
-    }
-
-    for (int i = rows - 1; i >= 0; i--) {
-        for (int j = cols - 1; j >= 1; j--)
-            if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                matrix[i][j] = 0;
-        if (col0 == 0) matrix[i][0] = 0;
-    }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*int rows=matrix.size();
-        int cols=matrix[0].size();
+           
+        int i,j;
+        int rows=M.size();
+        int cols=M[0].size();
         
         int col0=1;
-        
+        // setting the values of dummy array
         for(int i=0;i<rows;i++)
         {
-            // checking the 0th columns of every row and if 0 found on 1st column making 
-            //col0=0(false)
+            //checking if 0th col of any row is 0 or not
+            if(M[i][0]==0)
+                col0=0;
             
-                if(matrix[i][0]==0)
-                    col0=0;
-                
             for(int j=1;j<cols;j++)
             {
-                if(matrix[i][j]==0)
+                if(M[i][j]==0)
                 {
-                    matrix[i][0]=matrix[0][j]=0;
+                    M[0][j]=0;
+                    M[i][0]=0;
                 }
             }
         }
         
-        //making chnages in 2d matrix now in reverse order
+        // now traverse the array in reverse and set the whole array in 0 according to dummy array values;
         
         for(int i=rows-1;i>=0;i--)
         {
-            for(int j=cols-1;j>=1;j--)
+            for(int j= cols-1;j>=1;j--)
             {
-                if(matrix[0][j]==0 || matrix[i][0]==0)
-                {
-                    matrix[i][j]=0;
-                }
+                //for rows and col
+                if(M[i][0]==0 || M[0][j]==0)
+                    M[i][j]=0;
             }
-             if(col0=0)
-                matrix[i][0]=0;
+            //for col0; special case
+                if(col0==0)
+                {
+                    M[i][0]=0;
+                }
         }
-        */
+        
+    
     }
+        
 };
