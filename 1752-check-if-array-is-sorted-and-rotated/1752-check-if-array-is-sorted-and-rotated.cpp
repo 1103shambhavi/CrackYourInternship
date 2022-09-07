4d43103151354a15f8f7ac4085c0c7fa99639791
Shambhavi Gupta <104186704+1103shambhavi@users.noreply.github.com>
2022-09-07 22:45:03 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (38.16%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     bool check(vector<int>& arr) 
     {
-        // the main logic is that if a the contains more than 1 pair for which arr[i-1]>arr[i] then the array is not sorted and rotated 
         
-        // check for these pairs in array traversal and 
-        // check seperately for arr[0]and arr[n]
         int n= arr.size();
-        int count=0;
-        if(arr[n-1]> arr[0])
-            count++;
-        
-        for(int i=1;i<n;i++)
+        int cnt=0;
+        for(int i=0;i<n-1;i++)
         {
-            if(arr[i-1]>arr[i] )
-                count++;
+            if(arr[i]>arr[i+1])
+                cnt++;
         }
-        if(count>1)
-            return false;
-        else
-            return true;
         
+        //checking last and 1st pair
+        if(arr[n-1]>arr[0])
+            cnt++;
+        
+        
+        if(cnt>1)
+            return false;
+        
+        else 
+            return true;
+            
+     
     }
 };
