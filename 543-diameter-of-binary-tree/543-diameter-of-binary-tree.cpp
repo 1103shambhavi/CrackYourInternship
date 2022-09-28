520e37a047596a81d8387afcd94c79ace12cf743
Shambhavi Gupta <104186704+1103shambhavi@users.noreply.github.com>
2022-09-28 18:27:12 +0530

    Time: 41 ms (6.21%), Space: 20.3 MB (64.55%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,38 +11,34 @@
  */
 class Solution {
     private:
-        int height(TreeNode* root)
-        {
-            if(root==NULL)
-                return 0;
-            
-            int h1= height(root->left);
-            int h2= height(root->right);
-            
-            int ans= max(h1,h2)+1;
-            return ans;
-        }
+    int height(TreeNode* root)
+    {
+        if(root==NULL)
+            return 0;
+        
+        //call for left
+        int h1= height(root->left);
+        int h2= height(root->right);
+        
+        int ans=max(h1,h2)+1;
+        
+        return ans;
+    }
     
 public:
     int diameterOfBinaryTree(TreeNode* root) 
     {
-        int val=0;
         //base case
         if(root==NULL)
             return 0;
         
-        //case 1 diameter in left half
-        int op1=diameterOfBinaryTree(root->left);
+        //call of left part
+        int left = diameterOfBinaryTree(root->left);
+        int right = diameterOfBinaryTree(root->right);
         
+        int both = height(root->left) + height(root->right);
         
-        
-        //case 2 diameter in right half
-        int op2= diameterOfBinaryTree(root->right);
-        
-        //case 3 diameter in both half
-        int op3=height(root->left)+ height(root->right); 
-        
-        val= max(op1, max(op2,op3));
-        return val;
+        int ans= max(left ,max(right , both));
+        return ans;
     }
 };
