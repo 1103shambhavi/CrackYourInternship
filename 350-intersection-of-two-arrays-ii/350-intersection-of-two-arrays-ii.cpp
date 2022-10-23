6b0235503341c7d95cb533d8f110de88ade3296d
Shambhavi Gupta <104186704+1103shambhavi@users.noreply.github.com>
2022-10-23 19:36:08 +0530

    Time: 21 ms (9.95%), Space: 10.3 MB (51.52%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -3,18 +3,19 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
     {
         vector<int>ans;
-        
+        unordered_map<int,int>fre;
         for(int i=0;i<nums1.size();i++)
         {
-            
-            for(int j=0;j<nums2.size();j++)
+            fre[nums1[i]]++;
+           
+        }
+        
+        for(int j=0;j<=nums2.size()-1;j++)
+        {
+            if(fre.find(nums2[j])!=fre.end() && fre[nums2[j]]>0)
             {
-                if(nums1[i] == nums2[j] )
-                {
-                    ans.push_back(nums2[j]);
-                    nums2[j]=-1;
-                    break;
-                }
+                ans.push_back(nums2[j]);
+                fre[nums2[j]]--;
             }
         }
         return ans;
