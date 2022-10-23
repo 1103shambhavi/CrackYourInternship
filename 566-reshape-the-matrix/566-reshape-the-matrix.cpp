bd25f5d1a335a4a2ca607317550bde6d11696142
Shambhavi Gupta <104186704+1103shambhavi@users.noreply.github.com>
2022-10-23 22:31:39 +0530

    Time: 16 ms (70.93%), Space: 11.3 MB (21.04%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int m=mat.size(),n=mat[0].size(),x=0,y=0;
-        vector<vector<int>>result(r,vector<int>(c));
-        if(n*m !=r*c) return mat;
-        for(int i=0;i<m;i++)
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) 
+    {
+        vector<vector<int>>ans(r, vector<int>(c));
+        
+        if(r*c != mat.size()*mat[0].size())
+            return mat;
+        
+        int row=0;
+        int col=0;
+        for(int i=0;i<mat.size();i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<mat[0].size();j++)
             {
-                result[x][y++]=mat[i][j];
-                if(y==c)
+                ans[row][col]=mat[i][j];
+                col++;
+                //if you rach at the end of row ie at last col increase the row                         and reset the col to 0
+                
+                if(col ==c)
                 {
-                    x++;
-                    y=0;
-                }    
+                    row++;
+                    col=0;
+                }
             }
         }
-        return result;
+        return ans;
     }
 };
