af37e4d565b3a45cf01812bbd49bcb2951263905
Shambhavi Gupta <104186704+1103shambhavi@users.noreply.github.com>
2022-10-24 10:44:31 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (70.32%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
-        ans.push_back({1});
-        int k=0;
-        numRows = numRows-1;
-        while(numRows){
-            vector<int> temp ;
-            temp.push_back(1);
-            for(int i =1;i<ans[k].size();i++){
-                temp.push_back(ans[k][i] +ans[k][i-1]);
-            }
-            temp.push_back(1);
-            k++;
-            ans.push_back(temp);
-            numRows = numRows-1;
+    vector<vector<int>> generate(int numRows) 
+    {
+        vector<vector<int>> ans(numRows);
+        
+        for(int i=0;i<numRows;i++)
+        {
+            ans[i].resize(i+1);
+            //making the 1st and last entry to 1 
+            ans[i][0]=ans[i][i]=1;
+            
+            //assigning the values of rest of columns
+            //iterating from col 1 to col i-1;
+            
+            for(int j=1;j<i;j++)
+                ans[i][j]= ans[i-1][j-1] + ans[i-1][j];
         }
+        
         return ans;
+        
+        
+       /* vector<vector<int>> r(numRows);
+
+        for (int i = 0; i < numRows; i++) {
+            r[i].resize(i + 1);
+            r[i][0] = r[i][i] = 1;
+  
+            for (int j = 1; j < i; j++)
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+        }
+        
+        return r;
+        */
     }
 };
