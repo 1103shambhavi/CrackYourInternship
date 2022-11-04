757cb34a0f751d4fbe2405e452ed118f9404e423
Shambhavi Gupta <104186704+1103shambhavi@users.noreply.github.com>
2022-11-04 19:51:55 +0530

    Time: 16 ms (49.29%), Space: 8 MB (27.79%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -1,66 +1,33 @@
 class Solution {
+    private:
+    bool isvowel(char ch)
+    {
+        if((ch == 'a'||ch == 'e'||ch == 'i'||ch == 'o'||ch == 'u') ||                     (ch == 'A'||ch == 'E'||ch == 'I'||ch == 'O'||ch == 'U'))
+           return true;
+           
+           else
+           return false;
+    }
+    
 public:
     string reverseVowels(string s) 
     {
-       int i=0;
-        int j=s.size()-1;
+        vector<char>vowel;
+       for(auto i :s)
+       {
+           if(isvowel(i))
+               vowel.push_back(i);
+       }
         
-        while(i<=j)
+       reverse(vowel.begin(),vowel.end());
+        
+        int cnt=0;
+        for(int i=0;i<s.size();i++)
         {
-            if
-                ((s[i] == 'a'||s[i] == 'e'||s[i] == 'i'||s[i] == 'o'||s[i] == 'u' ||                     (s[i] == 'A'||s[i] == 'E'||s[i] == 'I'||s[i] == 'O'||s[i] == 'U'))
-               &&
-                ((s[j] == 'a'||s[j] == 'e'||s[j] == 'i'||s[j] == 'o'||s[j] == 'u')
-              ||(s[j] == 'A'||s[j] == 'E'||s[j] == 'I'||s[j] == 'O'||s[j] == 'U')) 
-              )
-                
+            if(isvowel(s[i]))
             {
-                swap(s[i],s[j]);
-                i++;
-                j--;
+                s[i]=vowel[cnt++];
             }
-            
-            else if(
-                ((s[i] != 'a'||s[i] != 'e'||s[i] != 'i'||s[i] != 'o'||s[i] != 'u') 
-                ||                         
-                (s[i] != 'A'||s[i] != 'E'||s[i] != 'I'||s[i] != 'O'||s[i] != 'U'))
-                
-               &&
-                ((s[j] == 'a'||s[j] == 'e'||s[j] == 'i'||s[j] == 'o'||s[j] == 'u')
-                 
-                   ||
-                 (s[j] == 'A'||s[j] == 'E'||s[j] == 'I'||s[j] == 'O'||s[j] =='U'))
-                )
-                
-            {
-                i++;
-            }
-            
-            
-            else if(
-                (
-                (s[i] == 'a'||s[i] == 'e'||s[i] == 'i'||s[i] == 'o'||s[i] == 'u')
-                || 
-                (s[i] == 'A'||s[i] == 'E'||s[i] == 'I'||s[i] == 'O'||s[i] == 'U')
-                )
-               &&
-                (
-                    (s[j] != 'a'||s[j] != 'e'||s[j] != 'i'||s[j] != 'o'||s[j] != 'u')
-                    ||
-                    (s[j] != 'A'||s[j] != 'E'||s[j] != 'I'||s[j] != 'O'||s[j] !='U')
-                 )   
-            )
-            {
-                j--;
-            }
-            
-            else
-            {
-                i++;
-                j--;
-            }
-            
-            
         }
         return s;
     }
