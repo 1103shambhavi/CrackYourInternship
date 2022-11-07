4990ac02993ee41ce3537547209bf0ce708d6fdf
Shambhavi Gupta <104186704+1103shambhavi@users.noreply.github.com>
2022-11-07 07:17:32 +0530

    Time: 108 ms (18.19%), Space: 36.8 MB (66.81%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -10,35 +10,49 @@
  * };
  */
 class Solution {
-public:
-   void storeInorder(TreeNode* root, vector<int> &inorder){
-    if(root == NULL)
+    private:
+    
+    void inorder(TreeNode* root , vector<int> &v)
+    {
+        //base case
+        if(root==NULL)
+            return ;
+        
+        //left
+        inorder(root->left , v);
+        //root
+        v.push_back(root->val);
+        //right
+        inorder(root->right , v);
+        
         return;
-    
-    storeInorder(root->left , inorder);
-    inorder.push_back(root->val);
-    storeInorder(root->right,inorder);
-    
-}
-bool findTarget(TreeNode* root, int k) {
-    vector<int> inorder;
-    
-    storeInorder(root,inorder);
-    
-    int s = 0 , e = inorder.size() -1;
-    
-    while( s < e ){
-        if( inorder[s] + inorder[e] == k ){
-            return true;
-        }
-        else if( inorder[s] + inorder[e] < k ){
-            s++;
-        }
-        else{
-            e--;
-        }
+        
     }
-    
-    return false;
-}
+public:
+    bool findTarget(TreeNode* root, int k) 
+    {
+        vector<int>v;
+        inorder(root,v);
+        
+        int i=0;
+        int j = v.size()-1;
+        
+        while(i<j)
+        {
+            if(v[i]+v[j] == k)
+            {
+                return true;
+                break;
+            }    
+            
+            else if(v[i]+v[j] > k)
+                j--;
+            
+            else
+                i++;
+        }
+        return false;
+        
+        
+    }
 };
